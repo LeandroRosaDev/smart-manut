913f0a196e6f7062c1f39581a34730b427f49cca
LeandroRosaDev <LeandroRosaDev>
2024-07-09 18:33:12 -0300

    Ajustando header

--- src/componentes/Header/Header.tsx
+++ src/componentes/Header/Header.tsx
@@ -19,28 +19,32 @@ export default function Header() {
   return (
     <section>
       {isAuthenticated && (
-        <header className="flex items-center justify-between p-4 bg-green-700 shadow-md ">
-          <div className="flex items-center ">
-            <GetProfilePicture width="60" height="60" />
-            <div className="ml-4 text-white">
-              <h1 className="text-lg font-semibold">{user?.nome}</h1>
-              <p className="text-sm text-white">{user?.email}</p>
+        <div className="flex flex-col gap-2">
+          <div className="w-full h-4 bg-green-700 fixed"></div>
+
+          <header className="flex items-center justify-between px-4 py-1 mt-4 bg-green-700 shadow-md ">
+            <div className="flex items-center ">
+              <GetProfilePicture width="60" height="60" />
+              <div className="ml-4 text-white">
+                <h1 className="text-lg font-semibold">{user?.nome}</h1>
+                <p className="text-sm text-white">{user?.email}</p>
+              </div>
             </div>
-          </div>
-          <form onSubmit={handleLogout} className="flex items-center">
-            <button
-              type="submit"
-              className="p-2 rounded-full bg-gray-100 hover:bg-gray-200"
-            >
-              <Image
-                src="/assets/icones/logout.svg"
-                width={24}
-                height={24}
-                alt="Logout Icon"
-              />
-            </button>
-          </form>
-        </header>
+            <form onSubmit={handleLogout} className="flex items-center">
+              <button
+                type="submit"
+                className="p-2 rounded-full bg-gray-100 hover:bg-gray-200"
+              >
+                <Image
+                  src="/assets/icones/logout.svg"
+                  width={24}
+                  height={24}
+                  alt="Logout Icon"
+                />
+              </button>
+            </form>
+          </header>
+        </div>
       )}
     </section>
   );
